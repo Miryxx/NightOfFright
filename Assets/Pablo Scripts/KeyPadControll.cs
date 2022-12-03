6a71d1a5e1bda035cf453dab4657dbcbfb00c5bf
jval1014 <77355756+jval1014@users.noreply.github.com>
2022-12-02 23:00:55 -0500

    Added code

---
+++ Assets/Pablo Scripts/KeyPadControll.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class KeyPadControll : MonoBehaviour
+{
+
+    public int correctCombination;
+    public bool accessGranted = false;
+
+    //Could be anything else
+    public Light spotLight;
+
+    private void Start()
+    {
+        spotLight.enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(accessGranted == true)
+        {
+            //Do whatever you action you want to take when the user hits the correct key.
+            spotLight.enabled = true;
+            accessGranted = false;
+        }
+    }
+
+
+    public bool CheckIfCorrect(int combination)
+    {
+        if(combination == correctCombination)
+        {
+            accessGranted = true;
+            return true;
+        }
+        return false;
+    }
+}
