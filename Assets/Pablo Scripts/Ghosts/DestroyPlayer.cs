22127006742a33194bcae36ee8a4988635bea367
jval1014 <77355756+jval1014@users.noreply.github.com>
2022-12-05 00:42:20 -0500

    DOOOOOOOONE

--- Assets/Pablo Scripts/Ghosts/DestroyPlayer.cs
+++ Assets/Pablo Scripts/Ghosts/DestroyPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DestroyPlayer : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class DestroyPlayer : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            Destroy(other.gameObject);
+            SceneManager.LoadScene(3);
         }
     }
 }
